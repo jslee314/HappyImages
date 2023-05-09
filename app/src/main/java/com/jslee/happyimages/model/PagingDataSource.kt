1278722ff38063ae81c073ac5fa4b9f0734e0f54
jslee314 <jslee_314@naver.com>
2023-05-09 15:26:58 +0900

    paging 기능 완료

---
+++ app/src/main/java/com/jslee/happyimages/model/PagingDataSource.kt
@@ -0,0 +1,66 @@
+package com.jslee.happyimages.model
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.jslee.happyimages.framework.Service
+import kotlinx.coroutines.delay
+import java.lang.Exception
+
+class PagingDataSource(val service: Service) : PagingSource<Int, Picsum>() {
+
+    val NETWORK_PAGE_SIZE = 9
+    val INITIAL_LOAD_SIZE = 1
+
+    override fun getRefreshKey(state: PagingState<Int, Picsum>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Picsum> {
+        return try {
+            delay(1000) // 1초의 딜레이
+
+            /* 서버와 연동할 때, */
+
+            val position = params.key ?: INITIAL_LOAD_SIZE
+            val offset = if (params.key != null) ((position - 1) * NETWORK_PAGE_SIZE) + 1 else INITIAL_LOAD_SIZE
+
+            val jsonResponse = service.getImageList(page = offset, limit = params.loadSize)
+            val nextKey = if (jsonResponse.isEmpty()) {
+                null
+            } else {
+                // initial load size = 3 * NETWORK_PAGE_SIZE
+                // ensure we're not requesting duplicating items, at the 2nd request
+                position + (params.loadSize / NETWORK_PAGE_SIZE)
+            }
+            LoadResult.Page(
+                data = jsonResponse,
+                prevKey = null, // Only paging forward.
+                nextKey = nextKey
+            )
+//
+//
+//            val pageId = params.key?: 1
+//            val myModelList = service.getImageList(pageId)
+//
+//            if(myModelList.isNotEmpty()) {
+//                LoadResult.Page(
+//                    data = myModelList,
+//                    prevKey = null,
+//                    nextKey = pageId + 1
+//                )
+//            } else {
+//                LoadResult.Page(
+//                    data = myModelList,
+//                    prevKey = null,
+//                    nextKey = null
+//                )
+//            }
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+    }
+
+}
